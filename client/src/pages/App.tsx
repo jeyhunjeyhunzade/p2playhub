796dcbf5ab66fa88ec30006db2c6f63e73146ade
Jeyhun Jeyhunzade <mrjeyhun.work@gmail.com>
2023-08-18 18:33:53 +0300

    fixed turn join game functionality and redesigned game board for chess game

--- client/src/pages/App.tsx
+++ client/src/pages/App.tsx
@@ -1,31 +1,30 @@
-import React, { useState } from "react";
-import {
-  BrowserRouter,
-  Routes,
-  Route,
-  Navigate,
-  Outlet,
-} from "react-router-dom";
-import { io } from "socket.io-client";
+import React, { createContext, useState } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import { AppRoutes } from "@app/types/enums";
 import Main from "./Main";
 import TicTac from "./Tictac";
 import Chess from "./Chess";
+import { AppContextShape } from "@app/types/types";
 
-const PrivateRoute = ({ user }: any) => {
-  return user ? <Outlet /> : <Navigate to={AppRoutes.chess} />;
-};
+export const AppContext = createContext<AppContextShape | null>(null);
 
 const App = () => {
+  const [userName, setUserName] = useState("");
+  const [isRoomActive, setIsRoomActive] = useState(false);
+
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path={AppRoutes.main} element={<Main />} />
-        <Route path={AppRoutes.tictac} element={<TicTac />} />
-        <Route path={AppRoutes.chess} element={<Chess />} />
-      </Routes>
-    </BrowserRouter>
+    <AppContext.Provider
+      value={{ userName, setUserName, isRoomActive, setIsRoomActive }}
+    >
+      <BrowserRouter>
+        <Routes>
+          <Route path={AppRoutes.main} element={<Main />} />
+          <Route path={AppRoutes.tictac} element={<TicTac />} />
+          <Route path={AppRoutes.chess} element={<Chess />} />
+        </Routes>
+      </BrowserRouter>
+    </AppContext.Provider>
   );
 };
 
